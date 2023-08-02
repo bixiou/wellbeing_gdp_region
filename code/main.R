b1a5d1a4bfa91426543f18899571ebac65a95b7c
Adrien Fabre <fabre.adrien@hotmail.fr>
2023-08-02 10:34:37 +0200

    simplification attempt

--- code/main.R
+++ code/main.R
@@ -14,6 +14,7 @@ View(w7)
 # TODO: (moins prioritaire que graphiques sur R) regarder les autres vagues.
 # TODO: creer colonne country
 # TODO: reproduire l'analyse économétrique!
+# TODO: trouver définitions de region qui correspondent aux nôtres
 
 # defining the regions
 region <- list(
@@ -62,25 +63,35 @@ a7$happiness_Layard <- (a7$happy + a7$satisfied)/2
 
 # econometric analysis
 # rearranging alternative GDP variable  (Y^)
-a7$log_gdp <- log(a7$gdp_pc)
+a7$log_gdp <- log10(a7$gdp_pc)
 a7$ranked_gdp <- rank(a7$gdp_pc)
-a7$ranked_log_gdp <- rank(a7$log_gdp)
-a7$gdp_group <- as.character(cut(a7$ranked_log_gdp, breaks = 6, labels = FALSE))
+# a7$ranked_log_gdp <- rank(a7$log_gdp)
+a7$gdp_group <- as.character(cut(a7$ranked_gdp, breaks = 6, labels = FALSE))
 #1 being the lowest and 6 being the highest gdp group (corresponding to the y_6ile variable in the previous paper)
 # clustered Y variables (corresponding to Y_clus4, Y_clus5, Y_clus6 and Y_clus7 in the previous paper)
 k_values <- c(4, 5, 6, 7)
 cluster_assignments <- list()
 for (k in k_values) {
   kmeans_result <- kmeans(a7$log_gdp, centers = k)
-  cluster_assignments[[paste0("gdp_cluster", k)]] <- as.character(kmeans_result$cluster)
+  # cluster_assignments[[paste0("gdp_cluster", k)]] <- as.character(kmeans_result$cluster)
+  a7[[paste0("gdp_cluster", k)]] <- as.character(kmeans_result$cluster)
 }
-a7$gdp_cluster4 <- cluster_assignments$gdp_cluster4
-a7$gdp_cluster5 <- cluster_assignments$gdp_cluster5
-a7$gdp_cluster6 <- cluster_assignments$gdp_cluster6
-a7$gdp_cluster7 <- cluster_assignments$gdp_cluster7
+# a7$gdp_cluster4 <- cluster_assignments$gdp_cluster4
+# a7$gdp_cluster5 <- cluster_assignments$gdp_cluster5
+# a7$gdp_cluster6 <- cluster_assignments$gdp_cluster6
+# a7$gdp_cluster7 <- cluster_assignments$gdp_cluster7
 View(a7)
 
 # regressions
+happiness_variables <- c("very_happy", "happy", "very_unhappy", "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard")
+regressions <- list()
+for (i in happiness_variables) {
+  regressions[[i]] <- list("zg" = lm(as.formula(paste(i, "~ region")), data = a7),
+                           "gdp" = lm(as.formula(paste(i, "~ log_gdp + I(log_gdp^2)")), data = a7),
+                           "gdp_group" = lm(as.formula(paste(i, "~ gdp_group")), data = a7))
+  for (k in 4:7) regressions[[i]][[paste0("gdp_cluster", k)]] <- lm(as.formula(paste0(i, "~ gdp_cluster", k)), data = a7)
+}
+
 # Zone Geographique (ZG)
 very_happyZG <- lm(a7$very_happy ~ a7$region)
 happyZG <- lm(a7$happy ~ a7$region)
@@ -153,97 +164,116 @@ happiness_meanGDPcluster7 <- lm(a7$happiness_mean ~ a7$gdp_cluster7)
 happiness_LayardGDPcluster7 <- lm(a7$happiness_Layard ~ a7$gdp_cluster7)
 View(a7)
 
-result_table_log_gdp <- bind_rows(
-  glance(very_happyGDP) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "log_gdp"),
-  glance(happyGDP) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "log_gdp"),
-  glance(very_unhappyGDP) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "log_gdp"),
-  glance(very_happy_over_very_unhappyGDP) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "log_gdp"),
-  glance(satisfiedGDP) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "log_gdp"),
-  glance(satisfied_meanGDP) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "log_gdp"),
-  glance(happiness_meanGDP) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "log_gdp"),
-  glance(happiness_LayardGDP) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "log_gdp")
-)
-print(result_table_log_gdp)
-
-result_table_region <- bind_rows(
-  glance(very_happyZG) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "region"),
-  glance(happyZG) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "region"),
-  glance(very_unhappyZG) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "region"),
-  glance(very_happy_over_very_unhappyZG) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "region"),
-  glance(satisfiedZG) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "region"),
-  glance(satisfied_meanZG) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "region"),
-  glance(happiness_meanZG) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "region"),
-  glance(happiness_LayardZG) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "region")
-)
-print(result_table_region)
-
-result_table_gdp_group <- bind_rows(
-  glance(very_happyGDPgroup) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_group"),
-  glance(happyGDPgroup) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_group"),
-  glance(very_unhappyGDPgroup) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_group"),
-  glance(very_happy_over_very_unhappyGDPgroup) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_group"),
-  glance(satisfiedGDPgroup) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_group"),
-  glance(satisfied_meanGDPgroup) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_group"),
-  glance(happiness_meanGDPgroup) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_group"),
-  glance(happiness_LayardGDPgroup) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_group")
-)
-print(result_table_gdp_group)
-
-result_table_gdp_cluster4 <- bind_rows(
-  glance(very_happyGDPcluster4) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster4"),
-  glance(happyGDPcluster4) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster4"),
-  glance(very_unhappyGDPcluster4) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster4"),
-  glance(very_happy_over_very_unhappyGDPcluster4) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster4"),
-  glance(satisfiedGDPcluster4) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster4"),
-  glance(satisfied_meanGDPcluster4) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster4"),
-  glance(happiness_meanGDPcluster4) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster4"),
-  glance(happiness_LayardGDPcluster4) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster4")
-)
-print(result_table_gdp_cluster4)
-
-result_table_gdp_cluster5 <- bind_rows(
-  glance(very_happyGDPcluster5) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster5"),
-  glance(happyGDPcluster5) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster5"),
-  glance(very_unhappyGDPcluster5) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster5"),
-  glance(very_happy_over_very_unhappyGDPcluster5) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster5"),
-  glance(satisfiedGDPcluster5) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster5"),
-  glance(satisfied_meanGDPcluster5) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster5"),
-  glance(happiness_meanGDPcluster5) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster5"),
-  glance(happiness_LayardGDPcluster5) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster5")
-)
-print(result_table_gdp_cluster5)
-
-result_table_gdp_cluster6 <- bind_rows(
-  glance(very_happyGDPcluster6) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster6"),
-  glance(happyGDPcluster6) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster6"),
-  glance(very_unhappyGDPcluster6) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster6"),
-  glance(very_happy_over_very_unhappyGDPcluster6) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster6"),
-  glance(satisfiedGDPcluster6) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster6"),
-  glance(satisfied_meanGDPcluster6) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster6"),
-  glance(happiness_meanGDPcluster6) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster6"),
-  glance(happiness_LayardGDPcluster6) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster6")
-)
-print(result_table_gdp_cluster6)
-
-result_table_gdp_cluster7 <- bind_rows(
-  glance(very_happyGDPcluster7) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster7"),
-  glance(happyGDPcluster7) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster7"),
-  glance(very_unhappyGDPcluster7) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster7"),
-  glance(very_happy_over_very_unhappyGDPcluster7) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster7"),
-  glance(satisfiedGDPcluster7) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster7"),
-  glance(satisfied_meanGDPcluster7) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster7"),
-  glance(happiness_meanGDPcluster7) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster7"),
-  glance(happiness_LayardGDPcluster7) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster7")
-)
-print(result_table_gdp_cluster7)
-
-combined_results <- bind_rows(
-  gdp_cluster4 = result_table_gdp_cluster4,
-  gdp_cluster5 = result_table_gdp_cluster5,
-  gdp_cluster6 = result_table_gdp_cluster6,
-  gdp_cluster7 = result_table_gdp_cluster7,
-  gdp_group = result_table_gdp_group
-)
+result_tables <- combined_results <- data.frame() # Peut-être qu'il faut indiquer les noms des colonnes dans data.frame()
+for (j in names(regressions)) {
+  result_tables[[j]] <- list()
+  for (i in happiness_variables) {
+    result_tables[[j]] <- rbind(result_tables[[j]], glance(eval(str2expression(i))) %>% mutate(Dependent_Variable = i, Independent_Variable = j))
+  }
+  if (grepl("gdp_", j)) combined_results <- rbind(combined_results, result_tables[[j]])
+}
+# bind_rows(
+#   glance(very_happyGDP) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "log_gdp"),
+#   glance(happyGDP) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "log_gdp"),
+#   glance(very_unhappyGDP) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "log_gdp"),
+#   glance(very_happy_over_very_unhappyGDP) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "log_gdp"),
+#   glance(satisfiedGDP) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "log_gdp"),
+#   glance(satisfied_meanGDP) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "log_gdp"),
+#   glance(happiness_meanGDP) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "log_gdp"),
+#   glance(happiness_LayardGDP) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "log_gdp")
+# )
+# 
+# result_table_log_gdp <- bind_rows(
+#   glance(very_happyGDP) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "log_gdp"),
+#   glance(happyGDP) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "log_gdp"),
+#   glance(very_unhappyGDP) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "log_gdp"),
+#   glance(very_happy_over_very_unhappyGDP) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "log_gdp"),
+#   glance(satisfiedGDP) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "log_gdp"),
+#   glance(satisfied_meanGDP) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "log_gdp"),
+#   glance(happiness_meanGDP) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "log_gdp"),
+#   glance(happiness_LayardGDP) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "log_gdp")
+# )
+# print(result_table_log_gdp)
+# 
+# result_table_region <- bind_rows(
+#   glance(very_happyZG) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "region"),
+#   glance(happyZG) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "region"),
+#   glance(very_unhappyZG) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "region"),
+#   glance(very_happy_over_very_unhappyZG) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "region"),
+#   glance(satisfiedZG) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "region"),
+#   glance(satisfied_meanZG) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "region"),
+#   glance(happiness_meanZG) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "region"),
+#   glance(happiness_LayardZG) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "region")
+# )
+# print(result_table_region)
+# 
+# result_table_gdp_group <- bind_rows(
+#   glance(very_happyGDPgroup) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_group"),
+#   glance(happyGDPgroup) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_group"),
+#   glance(very_unhappyGDPgroup) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_group"),
+#   glance(very_happy_over_very_unhappyGDPgroup) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_group"),
+#   glance(satisfiedGDPgroup) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_group"),
+#   glance(satisfied_meanGDPgroup) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_group"),
+#   glance(happiness_meanGDPgroup) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_group"),
+#   glance(happiness_LayardGDPgroup) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_group")
+# )
+# print(result_table_gdp_group)
+# 
+# result_table_gdp_cluster4 <- bind_rows(
+#   glance(very_happyGDPcluster4) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster4"),
+#   glance(happyGDPcluster4) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster4"),
+#   glance(very_unhappyGDPcluster4) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster4"),
+#   glance(very_happy_over_very_unhappyGDPcluster4) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster4"),
+#   glance(satisfiedGDPcluster4) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster4"),
+#   glance(satisfied_meanGDPcluster4) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster4"),
+#   glance(happiness_meanGDPcluster4) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster4"),
+#   glance(happiness_LayardGDPcluster4) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster4")
+# )
+# print(result_table_gdp_cluster4)
+# 
+# result_table_gdp_cluster5 <- bind_rows(
+#   glance(very_happyGDPcluster5) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster5"),
+#   glance(happyGDPcluster5) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster5"),
+#   glance(very_unhappyGDPcluster5) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster5"),
+#   glance(very_happy_over_very_unhappyGDPcluster5) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster5"),
+#   glance(satisfiedGDPcluster5) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster5"),
+#   glance(satisfied_meanGDPcluster5) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster5"),
+#   glance(happiness_meanGDPcluster5) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster5"),
+#   glance(happiness_LayardGDPcluster5) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster5")
+# )
+# print(result_table_gdp_cluster5)
+# 
+# result_table_gdp_cluster6 <- bind_rows(
+#   glance(very_happyGDPcluster6) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster6"),
+#   glance(happyGDPcluster6) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster6"),
+#   glance(very_unhappyGDPcluster6) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster6"),
+#   glance(very_happy_over_very_unhappyGDPcluster6) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster6"),
+#   glance(satisfiedGDPcluster6) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster6"),
+#   glance(satisfied_meanGDPcluster6) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster6"),
+#   glance(happiness_meanGDPcluster6) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster6"),
+#   glance(happiness_LayardGDPcluster6) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster6")
+# )
+# print(result_table_gdp_cluster6)
+# 
+# result_table_gdp_cluster7 <- bind_rows(
+#   glance(very_happyGDPcluster7) %>% mutate(Dependent_Variable = "happy", Independent_Variable = "gdp_cluster7"),
+#   glance(happyGDPcluster7) %>% mutate(Dependent_Variable = "very_happy", Independent_Variable = "gdp_cluster7"),
+#   glance(very_unhappyGDPcluster7) %>% mutate(Dependent_Variable = "very_unhappy", Independent_Variable = "gdp_cluster7"),
+#   glance(very_happy_over_very_unhappyGDPcluster7) %>% mutate(Dependent_Variable = "very_happy_over_very_unhappy", Independent_Variable = "gdp_cluster7"),
+#   glance(satisfiedGDPcluster7) %>% mutate(Dependent_Variable = "satisfied", Independent_Variable = "gdp_cluster7"),
+#   glance(satisfied_meanGDPcluster7) %>% mutate(Dependent_Variable = "satisfied_mean", Independent_Variable = "gdp_cluster7"),
+#   glance(happiness_meanGDPcluster7) %>% mutate(Dependent_Variable = "happiness_mean", Independent_Variable = "gdp_cluster7"),
+#   glance(happiness_LayardGDPcluster7) %>% mutate(Dependent_Variable = "happiness_Layard", Independent_Variable = "gdp_cluster7")
+# )
+# print(result_table_gdp_cluster7)
+# 
+# combined_results <- bind_rows(
+#   gdp_cluster4 = result_table_gdp_cluster4,
+#   gdp_cluster5 = result_table_gdp_cluster5,
+#   gdp_cluster6 = result_table_gdp_cluster6,
+#   gdp_cluster7 = result_table_gdp_cluster7,
+#   gdp_group = result_table_gdp_group
+# )
 
 combined_results_max <- combined_results %>%
 group_by(Dependent_Variable) %>%
