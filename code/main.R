e5e787c569f63f7220ced61c3abd3970d14ff890
Adrien Fabre <fabre.adrien@hotmail.fr>
2023-08-02 18:15:09 +0200

    fix bug

--- code/main.R
+++ code/main.R
@@ -1,9 +1,4 @@
-WVS7 <- read_csv("WVS7.csv")
-w7 <- WVS7[, c("Q46", "Q49", "GDPpercap2", "B_COUNTRY_ALPHA", "W_WEIGHT")]
-names(w7) <- c("happiness", "satisfaction", "gdp_pc", "country_code", "weight")
-View(w7)
 # TODO: regarder si les donnees de GDP correspondent a la date de l'enquete, et le faire le cas echeant
-
 # TODO: analyse variance (anova, r2 total ≠ r2y + r2reg): reg_tot<- lm(b ~ y + r, data = a7)
 # variance_reg_tot <- calc.relimp(reg_tot, type = c("lmg"), rela = F, rank= F)
 # plot: requires function "barres". Not so useful in our case as we have only two variables
@@ -35,19 +30,28 @@ get_country_region <- function(country_code) {
   return("Other")
 }
 
-# new column "region"
-w7 <- w7 %>%
-  mutate(region = sapply(country_code, get_country_region))
+# Prepare  WVS7
+# WVS7 <- read_csv("WVS7.csv")
+# w7 <- WVS7[, c("Q46", "Q49", "GDPpercap2", "B_COUNTRY_ALPHA", "W_WEIGHT")]
+# names(w7) <- c("happiness", "satisfaction", "gdp_pc", "country_code", "weight")
+# View(w7)
+# 
+# # new column "region"
+# w7 <- w7 %>%
+#   mutate(region = sapply(country_code, get_country_region))
+# 
+# # adding missing values
+# w7$gdp_pc[w7$country_code == "AND"] <- 42903
+# w7$gdp_pc[w7$country_code == "IRN"] <- 13338
+# w7$gdp_pc[w7$country_code == "NIR"] <- 32226
+# w7$gdp_pc[w7$country_code == "TWN"] <- 25903
+# w7$gdp_pc[w7$country_code == "VEN"] <- 6106
+# w7$region[w7$country_code == "NIR"] <- "Western"
+# w7$region[w7$country_code == "MAC"] <- "Asia"
+# decrit("region", w7)
+# write.csv(w7, "../data/WVS7.csv", row.names = FALSE)
 
-# adding missing values
-w7$gdp_pc[w7$country_code == "AND"] <- 42903
-w7$gdp_pc[w7$country_code == "IRN"] <- 13338
-w7$gdp_pc[w7$country_code == "NIR"] <- 32226
-w7$gdp_pc[w7$country_code == "TWN"] <- 25903
-w7$gdp_pc[w7$country_code == "VEN"] <- 6106
-w7$region[w7$country_code == "NIR"] <- "Western"
-w7$region[w7$country_code == "MAC"] <- "Asia"
-decrit("region", w7)
+w7 <- read.csv("../data/WVS7.csv")
 
 # adding the weights and calculating the means
 a7 <- w7 %>% group_by(country_code) %>% 
@@ -80,7 +84,7 @@ for (k in k_values) {
 # a7$gdp_cluster5 <- cluster_assignments$gdp_cluster5
 # a7$gdp_cluster6 <- cluster_assignments$gdp_cluster6
 # a7$gdp_cluster7 <- cluster_assignments$gdp_cluster7
-View(a7)
+# View(a7)
 
 # regressions
 happiness_variables <- c("very_happy", "happy", "very_unhappy", "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard")
@@ -92,97 +96,98 @@ for (i in happiness_variables) {
   for (k in 4:7) regressions[[i]][[paste0("gdp_cluster", k)]] <- lm(as.formula(paste0(i, "~ gdp_cluster", k)), data = a7)
 }
 
-# Zone Geographique (ZG)
-very_happyZG <- lm(a7$very_happy ~ a7$region)
-happyZG <- lm(a7$happy ~ a7$region)
-very_unhappyZG <- lm(a7$very_unhappy ~ a7$region)
-very_happy_over_very_unhappyZG <- lm(a7$very_happy_over_very_unhappy ~ a7$region)
-satisfiedZG <- lm(a7$satisfied ~ a7$region)
-satisfied_meanZG <- lm(a7$satisfied_mean ~ a7$region)
-happiness_meanZG <- lm(a7$happiness_mean ~ a7$region)
-happiness_LayardZG <- lm(a7$happiness_Layard ~ a7$region)
+# # Zone Geographique (ZG)
+# very_happyZG <- lm(a7$very_happy ~ a7$region)
+# happyZG <- lm(a7$happy ~ a7$region)
+# very_unhappyZG <- lm(a7$very_unhappy ~ a7$region)
+# very_happy_over_very_unhappyZG <- lm(a7$very_happy_over_very_unhappy ~ a7$region)
+# satisfiedZG <- lm(a7$satisfied ~ a7$region)
+# satisfied_meanZG <- lm(a7$satisfied_mean ~ a7$region)
+# happiness_meanZG <- lm(a7$happiness_mean ~ a7$region)
+# happiness_LayardZG <- lm(a7$happiness_Layard ~ a7$region)
+# 
+# # GDP per capita (Y)
+# very_happyGDP <- lm(very_happy ~ log_gdp + I(log_gdp^2), data = a7)
+# happyGDP <- lm(happy ~ log_gdp + I(log_gdp^2), data = a7)
+# very_unhappyGDP <- lm(very_unhappy ~ log_gdp + I(log_gdp^2), data = a7)
+# very_happy_over_very_unhappyGDP <- lm(very_happy_over_very_unhappy ~ log_gdp + I(log_gdp^2), data = a7)
+# satisfiedGDP <- lm(satisfied ~ log_gdp + I(log_gdp^2), data = a7)
+# sartisfied_meanGDP <- lm(satisfied_mean ~ log_gdp + I(log_gdp^2), data = a7)
+# happiness_meanGDP <- lm(happiness_mean ~ log_gdp + I(log_gdp^2), data = a7)
+# happiness_LayardGDP <- lm(happiness_Layard ~ log_gdp + I(log_gdp^2), data = a7)
+# 
+# # GDP per capita groups (variantes de Y)
+# # Y_6ile (rearranged growing gdp and clusters)
+# very_happyGDPgroup <- lm(a7$very_happy ~ a7$gdp_group)
+# happyGDPgroup <- lm(a7$happy ~ a7$gdp_group)
+# very_unhappyGDPgroup <- lm(a7$very_unhappy ~ a7$gdp_group)
+# very_happy_over_very_unhappyGDPgroup <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_group)
+# satisfiedGDPgroup <- lm(a7$satisfied ~ a7$gdp_group)
+# satisfied_meanGDPgroup <- lm(a7$satisfied_mean ~ a7$gdp_group)
+# happiness_meanGDPgroup <- lm(a7$happiness_mean ~ a7$gdp_group)
+# happiness_LayardGDPgroup <- lm(a7$happiness_Layard ~ a7$gdp_group)
+# 
+# # Y_cluster4 (cluster of 4)
+# very_happyGDPcluster4 <- lm(a7$very_happy ~ a7$gdp_cluster4)
+# happyGDPcluster4 <- lm(a7$happy ~ a7$gdp_cluster4)
+# very_unhappyGDPcluster4 <- lm(a7$very_unhappy ~ a7$gdp_cluster4)
+# very_happy_over_very_unhappyGDPcluster4 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster4)
+# satisfiedGDPcluster4 <- lm(a7$satisfied ~ a7$gdp_cluster4)
+# satisfied_meanGDPcluster4 <- lm(a7$satisfied_mean ~ a7$gdp_cluster4)
+# happiness_meanGDPcluster4 <- lm(a7$happiness_mean ~ a7$gdp_cluster4)
+# happiness_LayardGDPcluster4 <- lm(a7$happiness_Layard ~ a7$gdp_cluster4)
+# 
+# # Y_cluster5 (cluster of 5)
+# very_happyGDPcluster5 <- lm(a7$very_happy ~ a7$gdp_cluster5)
+# happyGDPcluster5 <- lm(a7$happy ~ a7$gdp_cluster5)
+# very_unhappyGDPcluster5 <- lm(a7$very_unhappy ~ a7$gdp_cluster5)
+# very_happy_over_very_unhappyGDPcluster5 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster5)
+# satisfiedGDPcluster5 <- lm(a7$satisfied ~ a7$gdp_cluster5)
+# satisfied_meanGDPcluster5 <- lm(a7$satisfied_mean ~ a7$gdp_cluster5)
+# happiness_meanGDPcluster5 <- lm(a7$happiness_mean ~ a7$gdp_cluster5)
+# happiness_LayardGDPcluster5 <- lm(a7$happiness_Layard ~ a7$gdp_cluster5)
+# 
+# # Y_cluster6 (cluster of 6)
+# very_happyGDPcluster6 <- lm(a7$very_happy ~ a7$gdp_cluster6)
+# happyGDPcluster6 <- lm(a7$happy ~ a7$gdp_cluster6)
+# very_unhappyGDPcluster6 <- lm(a7$very_unhappy ~ a7$gdp_cluster6)
+# very_happy_over_very_unhappyGDPcluster6 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster6)
+# satisfiedGDPcluster6 <- lm(a7$satisfied ~ a7$gdp_cluster6)
+# satisfied_meanGDPcluster6 <- lm(a7$satisfied_mean ~ a7$gdp_cluster6)
+# happiness_meanGDPcluster6 <- lm(a7$happiness_mean ~ a7$gdp_cluster6)
+# happiness_LayardGDPcluster6 <- lm(a7$happiness_Layard ~ a7$gdp_cluster6)
+# 
+# # Y_cluster7 (cluster of 7)
+# very_happyGDPcluster7 <- lm(a7$very_happy ~ a7$gdp_cluster7)
+# happyGDPcluster7 <- lm(a7$happy ~ a7$gdp_cluster7)
+# very_unhappyGDPcluster7 <- lm(a7$very_unhappy ~ a7$gdp_cluster7)
+# very_happy_over_very_unhappyGDPcluster7 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster7)
+# satisfiedGDPcluster7 <- lm(a7$satisfied ~ a7$gdp_cluster7)
+# satisfied_meanGDPcluster7 <- lm(a7$satisfied_mean ~ a7$gdp_cluster7)
+# happiness_meanGDPcluster7 <- lm(a7$happiness_mean ~ a7$gdp_cluster7)
+# happiness_LayardGDPcluster7 <- lm(a7$happiness_Layard ~ a7$gdp_cluster7)
+# View(a7)
 
-# GDP per capita (Y)
-very_happyGDP <- lm(very_happy ~ log_gdp + I(log_gdp^2), data = a7)
-happyGDP <- lm(happy ~ log_gdp + I(log_gdp^2), data = a7)
-very_unhappyGDP <- lm(very_unhappy ~ log_gdp + I(log_gdp^2), data = a7)
-very_happy_over_very_unhappyGDP <- lm(very_happy_over_very_unhappy ~ log_gdp + I(log_gdp^2), data = a7)
-satisfiedGDP <- lm(satisfied ~ log_gdp + I(log_gdp^2), data = a7)
-sartisfied_meanGDP <- lm(satisfied_mean ~ log_gdp + I(log_gdp^2), data = a7)
-happiness_meanGDP <- lm(happiness_mean ~ log_gdp + I(log_gdp^2), data = a7)
-happiness_LayardGDP <- lm(happiness_Layard ~ log_gdp + I(log_gdp^2), data = a7)
+# happiness_variables <- c(
+#   "very_happy", "happy", "very_unhappy", "very_happy_over_very_unhappy",
+#   "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard"
+# )
 
-# GDP per capita groups (variantes de Y)
-# Y_6ile (rearranged growing gdp and clusters)
-very_happyGDPgroup <- lm(a7$very_happy ~ a7$gdp_group)
-happyGDPgroup <- lm(a7$happy ~ a7$gdp_group)
-very_unhappyGDPgroup <- lm(a7$very_unhappy ~ a7$gdp_group)
-very_happy_over_very_unhappyGDPgroup <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_group)
-satisfiedGDPgroup <- lm(a7$satisfied ~ a7$gdp_group)
-satisfied_meanGDPgroup <- lm(a7$satisfied_mean ~ a7$gdp_group)
-happiness_meanGDPgroup <- lm(a7$happiness_mean ~ a7$gdp_group)
-happiness_LayardGDPgroup <- lm(a7$happiness_Layard ~ a7$gdp_group)
-
-# Y_cluster4 (cluster of 4)
-very_happyGDPcluster4 <- lm(a7$very_happy ~ a7$gdp_cluster4)
-happyGDPcluster4 <- lm(a7$happy ~ a7$gdp_cluster4)
-very_unhappyGDPcluster4 <- lm(a7$very_unhappy ~ a7$gdp_cluster4)
-very_happy_over_very_unhappyGDPcluster4 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster4)
-satisfiedGDPcluster4 <- lm(a7$satisfied ~ a7$gdp_cluster4)
-satisfied_meanGDPcluster4 <- lm(a7$satisfied_mean ~ a7$gdp_cluster4)
-happiness_meanGDPcluster4 <- lm(a7$happiness_mean ~ a7$gdp_cluster4)
-happiness_LayardGDPcluster4 <- lm(a7$happiness_Layard ~ a7$gdp_cluster4)
-
-# Y_cluster5 (cluster of 5)
-very_happyGDPcluster5 <- lm(a7$very_happy ~ a7$gdp_cluster5)
-happyGDPcluster5 <- lm(a7$happy ~ a7$gdp_cluster5)
-very_unhappyGDPcluster5 <- lm(a7$very_unhappy ~ a7$gdp_cluster5)
-very_happy_over_very_unhappyGDPcluster5 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster5)
-satisfiedGDPcluster5 <- lm(a7$satisfied ~ a7$gdp_cluster5)
-satisfied_meanGDPcluster5 <- lm(a7$satisfied_mean ~ a7$gdp_cluster5)
-happiness_meanGDPcluster5 <- lm(a7$happiness_mean ~ a7$gdp_cluster5)
-happiness_LayardGDPcluster5 <- lm(a7$happiness_Layard ~ a7$gdp_cluster5)
-
-# Y_cluster6 (cluster of 6)
-very_happyGDPcluster6 <- lm(a7$very_happy ~ a7$gdp_cluster6)
-happyGDPcluster6 <- lm(a7$happy ~ a7$gdp_cluster6)
-very_unhappyGDPcluster6 <- lm(a7$very_unhappy ~ a7$gdp_cluster6)
-very_happy_over_very_unhappyGDPcluster6 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster6)
-satisfiedGDPcluster6 <- lm(a7$satisfied ~ a7$gdp_cluster6)
-satisfied_meanGDPcluster6 <- lm(a7$satisfied_mean ~ a7$gdp_cluster6)
-happiness_meanGDPcluster6 <- lm(a7$happiness_mean ~ a7$gdp_cluster6)
-happiness_LayardGDPcluster6 <- lm(a7$happiness_Layard ~ a7$gdp_cluster6)
-
-# Y_cluster7 (cluster of 7)
-very_happyGDPcluster7 <- lm(a7$very_happy ~ a7$gdp_cluster7)
-happyGDPcluster7 <- lm(a7$happy ~ a7$gdp_cluster7)
-very_unhappyGDPcluster7 <- lm(a7$very_unhappy ~ a7$gdp_cluster7)
-very_happy_over_very_unhappyGDPcluster7 <- lm(a7$very_happy_over_very_unhappy ~ a7$gdp_cluster7)
-satisfiedGDPcluster7 <- lm(a7$satisfied ~ a7$gdp_cluster7)
-satisfied_meanGDPcluster7 <- lm(a7$satisfied_mean ~ a7$gdp_cluster7)
-happiness_meanGDPcluster7 <- lm(a7$happiness_mean ~ a7$gdp_cluster7)
-happiness_LayardGDPcluster7 <- lm(a7$happiness_Layard ~ a7$gdp_cluster7)
-View(a7)
-
-happiness_variables <- c(
-  "very_happy", "happy", "very_unhappy", "very_happy_over_very_unhappy",
-  "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard"
-)
-
-result_tables <- combined_results <- data.frame("very_happy", "happy", "very_unhappy", "very_happy_over_very_unhappy",  "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard") # Peut-être qu'il faut indiquer les noms des colonnes dans data.frame()
-for (j in names(regressions)) {
+result_tables <- list()
+combined_results <- data.frame() # Peut-être qu'il faut indiquer les noms des colonnes dans data.frame()
+for (j in names(regressions[[1]])) {
   result_tables[[j]] <- list()
   for (i in happiness_variables) {
-    result_tables[[j]] <- rbind(result_tables[[j]], glance(eval(str2expression(i))) %>% mutate(Dependent_Variable = i, Independent_Variable = j))
+    result_tables[[j]] <- rbind(result_tables[[j]], glance(regressions[[i]][[j]]) %>% mutate(Dependent_Variable = i, Independent_Variable = j))
   }
   if (grepl("gdp_", j)) combined_results <- rbind(combined_results, result_tables[[j]])
 }
 
 
-happiness_variables <- c(
-  "very_happy", "happy", "very_unhappy", "very_happy_over_very_unhappy",
-  "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard"
-)
+# happiness_variables <- c(
+#   "very_happy", "happy", "very_unhappy", "very_happy_over_very_unhappy",
+#   "satisfied", "satisfied_mean", "happiness_mean", "happiness_Layard"
+# )
 
 result_tables <- data.frame(
   Dependent_Variable = character(),
